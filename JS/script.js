532816649ffa8a7cbd4db6afd74aa1aba4485dab
dzaky <dzakynaufalpr@gmail.com>
2024-07-04 23:52:21 +0700

    new config

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,47 @@
-let space = document.getElementsBy("space");
+//declare place
+//const nama = document.querySelector(".user-name"); 
+const body = document.body;
+const listImg = document.querySelectorAll('.list-img')
 
-let boy = "hai";
 
-s
+//greeting for visitor
+
+const paragraf = document.createElement('p');
+    paragraf.textContent = inputan;
+    if(inputan == '' || inputan.trim() == ''){
+    nama.append('user')
+}
+nama.append(inputan);
+
+//console.log(document.getElementsByClassName("lidocumen
+
+
+
+
+function hasil (){
+    let firstname = document.getElementById("firstname");
+    let lastname = document.getElementById("lastname");
+    let gender = document.querySelector('input[name="gender"]:checked');
+    let pesan = document.getElementById("pesan");
+    let f_target = document.querySelector('.first-name-target');
+    let l_target = document.querySelector('.last-name-target');
+    let pesan_target = document.querySelector('.pesan-target');
+    let jenis_kelamin = document.querySelector('.jenis-kelamin-target')
+
+    f_target.innerHTML = "Nama " +  firstname.value + " " + lastname.value;
+    jenis_kelamin.innerHTML = "jenis kelamin : " + gender.value;
+    pesan_target.innerHTML = "Pesan : " + pesan.value;
+    
+    firstname.value = "";
+    lastname.value = "";
+    gender.checked = false;
+    pesan.value = '';
+
+
+    
+}
+
+
+
+
 
