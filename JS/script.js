349bb92a5d9a4c55c2a1423baeb71a30b54dd733
dzaky <dzakynaufalpr@gmail.com>
2024-07-05 17:55:51 +0700

    new

--- JS/script.js
+++ JS/script.js
@@ -23,19 +23,24 @@ function hasil (){
     let lastname = document.getElementById("lastname");
     let gender = document.querySelector('input[name="gender"]:checked');
     let pesan = document.getElementById("pesan");
+    let tanggal = document.getElementById("birtday");
     let f_target = document.querySelector('.first-name-target');
     let l_target = document.querySelector('.last-name-target');
     let pesan_target = document.querySelector('.pesan-target');
     let jenis_kelamin = document.querySelector('.jenis-kelamin-target')
+    let t_target = document.querySelector('.tgl-target');
 
     f_target.innerHTML = "Nama " +  firstname.value + " " + lastname.value;
+    t_target.innerHTML = "Tanggal Lahir : " + tanggal.value;
     jenis_kelamin.innerHTML = "jenis kelamin : " + gender.value;
     pesan_target.innerHTML = "Pesan : " + pesan.value;
+
     
     firstname.value = "";
     lastname.value = "";
     gender.checked = false;
     pesan.value = '';
+    tanggal.value = false;
 
 
     
